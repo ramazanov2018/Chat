d98cbfb3c5bb7a7b615e9c8769640087fc2305af
Niyamutdin Ramazanov <n.ramazanov@runetsoft.ru>
2022-03-01 12:13:46 +0300

    Intent test

--- app/src/main/java/com/example/chat/LogIn.kt
+++ app/src/main/java/com/example/chat/LogIn.kt
@@ -1,11 +1,31 @@
 package com.example.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.chat.databinding.ActivityLogInBinding
+import com.example.chat.databinding.ActivitySignUpBinding
 
 class LogIn : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLogInBinding
+    var launcher: ActivityResultLauncher<Intent>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = ActivityLogInBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+        }
+        binding.signUp.setOnClickListener{
+            Log.d("MyLog", "Страница регистрации")
+            launcher?.launch(Intent(this, SignUp::class.java))
+        }
     }
 }
