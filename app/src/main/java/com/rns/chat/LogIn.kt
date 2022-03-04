3eb744a271847ce6e4004873fac0b8d043428030
rns <niyazanov@yandex.ru>
2022-03-05 00:26:06 +0300

    database connect

--- app/src/main/java/com/rns/chat/LogIn.kt
+++ app/src/main/java/com/rns/chat/LogIn.kt
@@ -1,17 +1,23 @@
 package com.rns.chat
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import com.rns.chat.databinding.ActivityLogInBinding
+
 
 class LogIn : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogInBinding
     var launcher: ActivityResultLauncher<Intent>? = null
+    private lateinit var database: DatabaseReference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,5 +31,12 @@ class LogIn : AppCompatActivity() {
             Log.d("MyLog", "Страница регистрации")
             launcher?.launch(Intent(this, SignUp::class.java))
         }
+
+        // Write a message to the database
+        // Write a message to the database
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
     }
 }
