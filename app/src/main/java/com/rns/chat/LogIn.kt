eeff4680b17376b703fc5e59fa646b2cee8cc51b
Niyamutdin Ramazanov <n.ramazanov@runetsoft.ru>
2022-03-17 09:45:33 +0300

    sign up

--- app/src/main/java/com/rns/chat/LogIn.kt
+++ app/src/main/java/com/rns/chat/LogIn.kt
@@ -2,7 +2,6 @@ package com.rns.chat
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
@@ -17,12 +16,14 @@ import com.rns.chat.databinding.ActivityLogInBinding
 class LogIn : AppCompatActivity() {
 
     private lateinit var binding: ActivityLogInBinding
-    var launcher: ActivityResultLauncher<Intent>? = null
+    var launcherSignUp: ActivityResultLauncher<Intent>? = null
+    var launcherMainActivity: ActivityResultLauncher<Intent>? = null
     private lateinit var auth: FirebaseAuth
 
     private lateinit var EdtEmail: EditText
     private lateinit var EdtPass: EditText
     private lateinit var btnLogIn: Button
+    private lateinit var btnSignUp: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,45 +31,40 @@ class LogIn : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        auth = FirebaseAuth.getInstance()
         EdtEmail = binding.edtEmail
         EdtPass = binding.edtPassword
         btnLogIn = binding.logIn
+        btnSignUp = binding.signUp
 
 
-        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+        launcherSignUp = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+        }
+        launcherMainActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
 
         }
 
 
-        binding.signUp.setOnClickListener{
-
-            /*auth.createUserWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this) { task ->
-                    if (task.isSuccessful) {
-                        // Sign in success, update UI with the signed-in user's information
-                        Log.d(TAG, "createUserWithEmail:success")
-                        val user = auth.currentUser
-                        updateUI(user)
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
-                        Toast.makeText(baseContext, "Authentication failed.",
-                            Toast.LENGTH_SHORT).show()
-                        updateUI(null)
-                    }
-                }*/
-
-
-            Toast.makeText(this, "Страница регистрации", Toast.LENGTH_SHORT).show()
-            Log.d("MyLog", "Страница регистрации")
-            launcher?.launch(Intent(this, SignUp::class.java))
+        btnSignUp.setOnClickListener{
+            launcherSignUp?.launch(Intent(this, SignUp::class.java))
         }
 
-        // Write a message to the database
-        // Write a message to the database
-        /*val database = Firebase.database
-        val myRef = database.getReference("  ")
+        btnLogIn.setOnClickListener{
+            val email = EdtEmail.text.toString();
+            val pass = EdtPass.text.toString();
+            LogIn(email, pass)
+        }
+    }
 
-        myRef.setValue("Hello, World!")*/
+    private fun LogIn(email: String, password:String){
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    launcherMainActivity?.launch(Intent(this, MainActivity::class.java))
+                } else {
+                    Toast.makeText(baseContext, "Неавторизовался", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
--- app/src/main/java/com/rns/chat/SignUp.kt
+++ app/src/main/java/com/rns/chat/SignUp.kt
@@ -1,11 +1,65 @@
 package com.rns.chat
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.rns.chat.databinding.ActivitySignUpBinding
 
 class SignUp : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+    var launcher: ActivityResultLauncher<Intent>? = null
+    private lateinit var auth: FirebaseAuth
+
+    private lateinit var EdtName: EditText
+    private lateinit var EdtEmail: EditText
+    private lateinit var EdtPass: EditText
+    private lateinit var btnSignUp: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        auth = FirebaseAuth.getInstance()
+        EdtEmail = binding.edtEmail
+        EdtPass = binding.edtPassword
+        EdtName = binding.edtName
+        btnSignUp = binding.sign
+
+        btnSignUp.setOnClickListener{
+            val name = EdtName.text.toString().trim()
+            val email = EdtEmail.text.toString().trim()
+            val pass = EdtPass.text.toString().trim()
+
+            SignUp(name, email, pass)
+        }
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+
+        }
+
+    }
+
+    private fun SignUp(name:String, email:String, pass:String){
+
+        auth.createUserWithEmailAndPassword(email, pass)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    launcher?.launch(Intent(this, MainActivity::class.java))
+                } else {
+                    Log.d("TAG", "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(baseContext, "email"+email+" pass"+ pass, Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 }
